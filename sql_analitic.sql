e25a39333401169f9fd90b1c311c90409078c591
g.shabalin <g.shabalin@lucky-team.pro>
2021-07-12 02:10:31 +0300

    analitic sql

---
+++ sql_analitic.sql
@@ -0,0 +1,9 @@
+SELECT 
+    resultlist.is_month as 'Месяц', links.link_name as 'Ссылка', resultlist.visit_count as 'Кол-во переходов', resultlist.position_on_top as 'Позиция в топе месяца по переходам' 
+FROM (SELECT 
+    *, row_number() over (partition by resultlist.link_id) as 'position_on_top' 
+FROM (SELECT 
+        YEAR(clicks.click_date) + '-' + MONTH(clicks.click_date) as 'is_month', clicks.link_id, count(*) as 'visit_count' 
+FROM 
+    clicks GROUP BY is_month, clicks.link_id) as resultlist ) as resultlist INNER JOIN links 
+WHERE resultlist.link_id = links.id ORDER BY resultlist.is_month DESC;
