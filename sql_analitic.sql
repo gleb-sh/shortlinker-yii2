e36d124a87547495cfe4777384744649a0898200
g.shabalin <g.shabalin@lucky-team.pro>
2021-07-12 02:47:01 +0300

    update sql query

--- sql_analitic.sql
+++ sql_analitic.sql
@@ -1,9 +1,9 @@
 SELECT 
     resultlist.is_month as 'Месяц', links.link_name as 'Ссылка', resultlist.visit_count as 'Кол-во переходов', resultlist.position_on_top as 'Позиция в топе месяца по переходам' 
 FROM (SELECT 
-    *, row_number() over (partition by resultlist.link_id) as 'position_on_top' 
+    *, row_number() over (partition by resultlist.is_month order by resultlist.visit_count DESC) as 'position_on_top' 
 FROM (SELECT 
         YEAR(clicks.click_date) + '-' + MONTH(clicks.click_date) as 'is_month', clicks.link_id, count(*) as 'visit_count' 
 FROM 
     clicks GROUP BY is_month, clicks.link_id) as resultlist ) as resultlist INNER JOIN links 
-WHERE resultlist.link_id = links.id ORDER BY resultlist.is_month DESC;
+WHERE resultlist.link_id = links.id ORDER BY resultlist.is_month DESC, resultlist.position_on_top ASC;
